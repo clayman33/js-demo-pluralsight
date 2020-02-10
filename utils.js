f2a9ba6e8ede8e191ca72b60507feb590c43a65f
charlievillacr <charlievilla@gmail.com>
2020-02-10 14:51:01 -0600

    JavaScript Getting Started project demo

---
+++ utils.js
@@ -0,0 +1,7 @@
+function showMessage(message) {
+    document.getElementById('message').textContent = message;
+}
+/* function showMessageTwo(messageTwo) {
+    document.getElementById('messageTwo').textContent = messageTwo;
+}
+*/
